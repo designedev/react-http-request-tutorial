a0941721090d6524f55aa5c84eaba33acaae719b
designedev <designedev@gmail.com>
2021-03-25 02:20:11 +0900

    added category-selectable news list component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div>
         <Categories category={category} onSelect={onSelect}/>
-        <NewsList/>
+        <NewsList category={category}/>
     </div>
   )
 }
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -8,15 +8,16 @@ const NewsListBlock = styled.div`
 
 
 
-const NewsList = ({data}) => {
+const NewsList = ({category}) => {
 
     const [articles, setArticles] = useState(null);
     const [loading, setLoading] = useState(false);
 
     const news_api_key = '31d584fe1e824388b5386ddab774050b';
+    const category_query = category === 'all' ? '' : `&category=${category}`
     const news_target_country_code = 'de';
     const news_api_url = 'https://newsapi.org/v2/top-headlines'
-    const full_request_url = `${news_api_url}?country=${news_target_country_code}&apiKey=${news_api_key}`
+    const full_request_url = `${news_api_url}?country=${news_target_country_code}${category_query}&apiKey=${news_api_key}`
 
     useEffect(() => {
         const setData = async () => {
@@ -31,7 +32,7 @@ const NewsList = ({data}) => {
             setLoading(false);
         }
         setData();
-    }, [full_request_url]);
+    }, [category, full_request_url]);
     if (loading) {
         return <NewsListBlock>Loading..</NewsListBlock>
     }
