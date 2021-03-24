e8e4661f78ed4a49b9da1927a1773700e686f804
designedev <designedev@gmail.com>
2021-03-24 18:21:25 +0900

    moved data loading functions to newslist component.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import axios from 'axios';
-import { useState } from 'react';
-import './App.css';
 import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const news_api_key = '31d584fe1e824388b5386ddab774050b';
-  const news_target_country_code = 'de';
-  const news_api_url = 'https://newsapi.org/v2/top-headlines'
-  const full_request_url = `${news_api_url}?country=${news_target_country_code}&apiKey=${news_api_key}`
-  const onClick = async() => {
-    try {
-      const response = await axios.get(
-        full_request_url
-        );
-      setData(response.data);
-    }
-    catch(error) {
-      console.log(error)
-    }
-  };
-
   return (
     <div>
-      <div>
-        <button onClick={onClick}>Load</button>
-      </div>
-      {data && (
-        <NewsList data={data}/>
-      )}
-      
+        <NewsList/>
     </div>
   )
 }
-
 export default App;
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,24 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 
 const NewsListBlock = styled.div`
 `
 
-const sampleArticle = {
-    title: 'title',
-    description: 'desc sdadsd asdada.. ',
-    url: 'https://www.google.com',
-    urlToImage: 'https://via.placeholder.com/160'
-};
+
 
 const NewsList = ({data}) => {
-    console.log(data.articles);
-    const articles = data.articles
+
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const news_api_key = '31d584fe1e824388b5386ddab774050b';
+    const news_target_country_code = 'de';
+    const news_api_url = 'https://newsapi.org/v2/top-headlines'
+    const full_request_url = `${news_api_url}?country=${news_target_country_code}&apiKey=${news_api_key}`
+
+    useEffect(() => {
+        const setData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(full_request_url);
+                setArticles(response.data.articles)
+            }
+            catch(err) {
+                console.log(err);
+            }
+            setLoading(false);
+        }
+        setData();
+    }, [full_request_url]);
+    if (loading) {
+        return <NewsListBlock>Loading..</NewsListBlock>
+    }
+    if (!articles) {
+        return null;
+    }
+
     return ( 
         <NewsListBlock>
-            {articles && articles.length > 0 && (
-                articles.map(item => <NewsItem article={item} />)
-            )}
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
         )
     }
